7af18046235da58c50925dc1e5e583ea3383b84c
mertcakmak <mertcakmak2@gmail.com>
2020-02-19 23:43:06 +0300

    blog commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <show-blogs></show-blogs>
+  </div>
+</template>
+
+<script>
+// import AddBlog from './components/AddBlog.vue'
+import ShowBlogs from './components/ShowBlogs.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // 'add-blog':AddBlog,
+    'show-blogs':ShowBlogs
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/ShowBlogs.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="show-blogs">
+        <h1>Makaleler</h1>
+        <div v-for="(data,index) in blogs" :key="index" class="single-blog">
+            <h3>{{data.title}}</h3>
+            <article>{{data.body}}</article>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'ShowBlogs',
+    data() {
+        return {
+            blogs:[]
+        }
+    },
+    created(){
+        this.$http.get('https://jsonplaceholder.typicode.com/posts',).then(function(data){
+            this.blogs = data.body.slice(0,10)
+        })
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
